76daf74bde92de1ae967674d7883e522143280dc
polohb <polohb@gmail.com>
2020-03-17 18:10:50 +0100

    improve script and add wireguard feature

--- README.md
+++ README.md
@@ -4,6 +4,17 @@ Scripts for [Argos](https://github.com/p-e-w/argos) gnome-shell extension.
 ## vpn-check.5s.sh
 Show the vpn status. 
 
-Only check if _tun0_ or _ipsec0_ or _wg0_ interface exist.
+Simply check if _tun0_ or _ipsec0_ or _wg0_ interface exist.
+
+
+### wireguard sepcificity
+This script also add a sub-menu to start and stop wireguard if `/usr/bin/wg-quick` exist.
+
+Note that wireguard as to be configured and saved to use the menu.
+
+To bypass the prompted password be sure that `/etc/sudoers.d/` is added in the _sudoers default file_ and create a `/etc/sudoers.d/sudo-vpn` file with the following content : 
+```
+%sudo  ALL=(ALL) NOPASSWD: /usr/bin/wg-quick
+```
 
 
--- vpn-check.5s.sh
+++ vpn-check.5s.sh
@@ -3,11 +3,28 @@
 # Desc : VPN Checker
 # check if tun0 ipsec0 or wg0 is present
 
+# To bypass the prompted password on start or stop
+# Be sure that /etc/sudoers.d/ is added in the sudoers default file
+# and create a /etc/sudoers.d/sudo-vpn file with the following content
+# %sudo  ALL=(ALL) NOPASSWD: /usr/bin/wg-quick
+
+
+WIREGUARD_CLI='/usr/bin/wg-quick'
 IFACE_LIST='tun0|ipsec0|wg0'
 
-
+# Check VPN status
 if ip a | grep -Eq ${IFACE_LIST}; then
 	echo "VPN ⬆ | color=green"
 else
 	echo "VPN ⬇ | color=red"
 fi
+
+# Add a menu to Start and Stop Wireguard if cli is found
+if [ -f "${WIREGUARD_CLI}" ]; then
+	echo "---"
+	echo "Start Wireguard | bash='sudo ${WIREGUARD_CLI} up wg0 && exit'"
+	echo "Stop Wireguard | bash='sudo ${WIREGUARD_CLI} down wg0 && exit'"
+fi
+
+
+
